5a5f98983487461fe69cf2003ad9d6798e7f6400
drazod <ngtienkhanh1702@gmail.com>
2025-09-19 03:43:43 +0700

    add HealthController with health check endpoint

---
+++ src/main/java/com/curcus/lms/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.curcus.lms.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+public class HealthController {
+    @GetMapping("/")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("OK");
+    }
+}
